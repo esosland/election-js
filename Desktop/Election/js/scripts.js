184bc632d9f84aed3ceca1ac529e4b86d2aebd66
Elissa Sosland and Kellie Olson <elissa@>
2016-03-22 10:49:08 -0700

    add portraits and clickble features for imgs

--- Desktop/Election/js/scripts.js
+++ Desktop/Election/js/scripts.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#clinton").slideToggle();
+  });
+  $(".clickable2").click(function() {
+    $("#trump").toggle();
+  });
+  $(".clickable3").click(function() {
+    $("#sanders").toggle();
+  });
+});
